84ad3512d5cf1e45a44d325759de221b942c4826
George Willson <drgeek3@gmail.com>
2025-04-24 11:01:18 -0500

    Added owned brick search function and modified to fire from enter key as well.

--- Clients/OwnedClient.cs
+++ Clients/OwnedClient.cs
@@ -6,12 +6,16 @@ namespace LegoCollectionUI.Clients
     {
         public async Task<OwnedBrick[]> GetOwnedBricksAsync() => await httpClient.GetFromJsonAsync<OwnedBrick[]>("legos/owned") ?? [];
 
+        public async Task<OwnedBrick[]> GetBrickIdAsync(string brickid)
+           => await httpClient.GetFromJsonAsync<OwnedBrick[]>($"legos/owned/brick/{brickid}")
+           ?? throw new Exception("Could not find brick.");
+
         public async Task AddBrickAsync(BrickDetails brick) 
             => await httpClient.PostAsJsonAsync("legos/owned", brick);
 
         public async Task<BrickDetails> GetBrickAsync(int id)
             => await httpClient.GetFromJsonAsync<BrickDetails>($"legos/owned/{id}")
-                ?? throw new Exception("Could not find game.");
+                ?? throw new Exception("Could not find brick.");
 
         public async Task UpdateBrickAsync(BrickDetails updatedBrick)
             => await httpClient.PutAsJsonAsync($"legos/owned/{updatedBrick.Id}", updatedBrick);
