52d03bdd0330a2b4aa4ae40369a60be077f7fcb4
George Willson <drgeek3@gmail.com>
2025-04-24 10:17:17 -0500

    Added search button to main brick list to search for brick id

--- Clients/OwnedClient.cs
+++ Clients/OwnedClient.cs
@@ -4,104 +4,20 @@ namespace LegoCollectionUI.Clients
 {
     public class OwnedClient(HttpClient httpClient)
     {
-        //private readonly List<OwnedBrick> bricks =
-        //[
-        //    new (){
-        //    Id = 1,
-        //    BrickId = "1234",
-        //    Color = "White",
-        //    Count = 28,
-        //    LocationId = "RA11"    },
-        //    new (){
-        //    Id = 2,
-        //    BrickId = "5678",
-        //    Color = "Red",
-        //    Count = 5,
-        //    LocationId = "RA15"    },
-        //    new (){
-        //    Id = 3,
-        //    BrickId = "29568",
-        //    Color = "Transparent Neon Orange",
-        //    Count = 5,
-        //    LocationId = "RA27"    },
-        //    new (){
-        //    Id = 4,
-        //    BrickId = "4122",
-        //    Color = "Transparent Blue",
-        //    Count = 31,
-        //    LocationId = "RA28"    }
-        //];
-
-        //private readonly ColorList[] colors = new ColorsClient(httpClient).GetColors();
-
         public async Task<OwnedBrick[]> GetOwnedBricksAsync() => await httpClient.GetFromJsonAsync<OwnedBrick[]>("legos/owned") ?? [];
 
         public async Task AddBrickAsync(BrickDetails brick) 
             => await httpClient.PostAsJsonAsync("legos/owned", brick);
-        //{
-        //    ColorList colorList = GetColorById(brick.ColorId);
-
-        //    var ownedBrick = new OwnedBrick
-        //    {
-        //        Id = bricks.Count + 1,
-        //        BrickId = brick.BrickId,
-        //        Color = colorList.Name,
-        //        Count = brick.Count,
-        //        LocationId = brick.LocationId
-        //    };
-
-        //    bricks.Add(ownedBrick);
-        //}
 
         public async Task<BrickDetails> GetBrickAsync(int id)
             => await httpClient.GetFromJsonAsync<BrickDetails>($"legos/owned/{id}")
                 ?? throw new Exception("Could not find game.");
-        //{
-        //    OwnedBrick? brick = GetBrickSummaryById(id);
-
-        //    var color = colors.Single(color => string.Equals(color.Name, brick.Color, StringComparison.OrdinalIgnoreCase));
-        //    return new BrickDetails
-        //    {
-        //        Id = id,
-        //        BrickId = brick.BrickId,
-        //        ColorId = color.Id.ToString(),
-        //        Count = brick.Count,
-        //        LocationId = brick.LocationId
-        //    };
-        //}
 
         public async Task UpdateBrickAsync(BrickDetails updatedBrick)
             => await httpClient.PutAsJsonAsync($"legos/owned/{updatedBrick.Id}", updatedBrick);
-        //{
-        //    var color = GetColorById(updatedBrick.ColorId);
-        //    OwnedBrick existingBrick = GetBrickSummaryById(updatedBrick.Id);
-
-        //    existingBrick.BrickId = updatedBrick.BrickId;
-        //    existingBrick.Color = color.Name;
-        //    existingBrick.Count = updatedBrick.Count;
-        //    existingBrick.LocationId = updatedBrick.LocationId;
-        //}
 
         public async Task DeleteBrickAsync(int id)
             => await httpClient.DeleteAsync($"legos/owned/{id}");
-        //{
-        //    var brick = GetBrickSummaryById(id);
-        //    bricks.Remove(brick);
-        //}
-
-        //private OwnedBrick GetBrickSummaryById(int id)
-        //{
-        //    var brick = bricks.Find(brick => brick.Id == id);
-        //    ArgumentNullException.ThrowIfNull(brick);
-        //    return brick;
-        //}
-
-        //private ColorList GetColorById(string? id)
-        //{
-        //    ArgumentException.ThrowIfNullOrWhiteSpace(id);
-        //    return colors.Single(colorList => colorList.Id == int.Parse(id));
-
-        //}
 
     }    
 }
